7a0502a7476057b645b7d33c1ce4551674d3bd85
YoussefHarizi <youssef.harizi@gmail.com>
2022-04-06 13:26:03 +0100

    fix first test

--- app/Http/Controllers/Api/V1/TodoListController.php
+++ app/Http/Controllers/Api/V1/TodoListController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\TodoList;
 use Illuminate\Http\Request;
 
 class TodoListController extends Controller
 {
     public function index()
     {
-        return response(['lists'=>[]]);
+        $lists=TodoList::all();
+        return response($lists);
     }
 }
--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,15 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
+    use RefreshDatabase;
 /** @test */
 public function fetch_todo_list()
 {
+    TodoList::create(['name'=>'first one']);
     $response=$this->get(route('todo-list.index'));
     $this->assertEquals(1,count($response->json()));
 }
